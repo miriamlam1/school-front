d0b405ce4051f82bc58b8c68b9a9de62acd06888
Miriam <mlam27@calpoly.edu>
2021-01-12 16:45:35 -0800

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UploadComponent } from './upload/upload.component';
+import { LabelComponent } from './label/label.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadComponent,
+    LabelComponent
   ],
   imports: [
     BrowserModule
